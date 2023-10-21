935926ba105e22d2317563b20feae768c3f0f47e
Solomon Odunusi <solomonodunusi@gmail.com>
2023-10-22 00:45:44 +0100

    I created a form input for creating a new blog

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,17 @@ const Create = () => {
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input type="text" required placeholder="Title"/>
+
+                <label>Blog: </label>
+                <textarea required placeholder="Blog"></textarea>
+
+                <label>Blog Author: </label>
+                <input type="text" required placeholder="Author"/>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
